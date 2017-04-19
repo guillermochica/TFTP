0b8b6cc8635eb6700aec0bc41042856648ef1a9c
guillermochica <1bgchica@gmail.com>
2017-04-19 20:20:38 +0200

    Add protocol

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,5 +1,89 @@
 package cliente;
 
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.util.Random;
+
+import protocolo.TFTP;
+
 public class Cliente {
+	
+	private DatagramSocket socket;
+	InetAddress ip;
+	int port;
+	Random r;
+
+	int dPort;
+
+	
+	
+	public Cliente(InetAddress ip) throws SocketException{
+		this.ip = ip;
+		int min = 49152;
+		int max = 65535;
+		port = r.nextInt();
+		port = port * (max-min) + min;
+		socket = new DatagramSocket(port, ip);
+	}
+	
+	
+	public void rrq(){
+		
+		
+	}
+	
+	public void wrq(String fileName) throws IOException{
+		
+		//Creo el paquete
+		DatagramPacket packet;
+		
+		byte[] request = TFTP.crearPeticion(fileName, "netascii", false);
+		
+		packet = new DatagramPacket(request,request.length,ip,69);
+		
+		//Envio paquete
+		socket.send(packet);
+		
+		//Recibe ACK
+		byte[] reception = new byte[4];
+		DatagramPacket rPacket = new DatagramPacket(reception, reception.length);
+		
+		socket.receive(rPacket);
+		
+		dPort = rPacket.getPort();
+		
+		//Conexion establecida, comienza envio datos
+		File file = new File(fileName);
+		
+		byte[] array = Files.readAllBytes(new File(fileName).toPath()); //path to file
+		byte[] datos = new byte[512];
+		
+		int blockn=0; //block number
+		for(int i=0, bytes=0 ; i<array.length ; i++, bytes++){
+			if(bytes==512){
+				//Creamos paquete y enviamos, ponemos bytes a 0
+			}
+			else{
+				//rellenamos array con datos
+				datos[bytes] = array[i];
+				//que pasa si no quedna mas datos?
+			}
+			
+		}
+		
+	}
+	
 
 }
--- src/protocolo/TFTP.java
+++ src/protocolo/TFTP.java
@@ -1,5 +1,114 @@
 package protocolo;
 
 public class TFTP {
+	
+	private static byte OPCODE_RRQ=1;
+	private static byte OPCODE_WRQ=2;
+	private static byte OPCODE_DATA=3;
+	private static byte OPCODE_ACK=4;
+	private static byte OPCODE_ERROR=5;
+	
+	private static byte zero = 0;
+	
+	public static byte[] crearPeticion(String fileName, String mode, boolean type){
+		/*Longitud de la peticion
+		 * 2 bytes de Opcode (1 byte 0 + 1 byte codigo) + longitud fileName + 1 byte 0 + longitud mode + 1 byte 0
+		 */
+		int rqByteLength = 2 + fileName.length() + 1 + mode.length() + 1;
+		
+		byte[] rqByteArray = new byte[rqByteLength];
+		
+		int position = 0; //recorre el array de bytes rrqByteArray
+		rqByteArray[position] = zero; //en la primera posicion esta el byte 0
+		position++;
+		
+		// Si type es true -> Read 
+		// Si type es false -> Write
+		if(type) {
+			rqByteArray[position] = OPCODE_RRQ;
+		}
+		else{
+			rqByteArray[position] = OPCODE_WRQ;
+		}
+		
+		position++;
+		for (int i = 0; i < fileName.length(); i++) {
+			rqByteArray[position] = (byte) fileName.charAt(i); //pasamos a byte cada caracter del fileName
+			position++;
+		}
+		rqByteArray[position] = zero;
+		position++;
+		for (int i = 0; i < mode.length(); i++) {
+			rqByteArray[position] = (byte) mode.charAt(i);
+			position++;
+		}
+		rqByteArray[position] = zero;
+		
+		return rqByteArray;
+	}
+	
+	public static byte[] crearPaqueteDatos(byte[] blockNumber, byte[] data){
+		
+		//Longitud: 2 bytes OPCode + 2 bytes blockNumber + n bytes data
+		
+		int packetLength = 2 + 2 + data.length; 
+		
+		byte[] dataByteArray = new byte[packetLength];
+		int posicion = 0;
+		dataByteArray[posicion] = 0;
+		posicion++;
+		dataByteArray[posicion] = OPCODE_DATA;
+		posicion++;
+		dataByteArray[posicion] = blockNumber[0];
+		posicion++;
+		dataByteArray[posicion] = blockNumber[1];
+		posicion++;
+		
+		for(int i=0;i<data.length ; i++) {
+			dataByteArray[posicion] = data[i];
+			posicion++;
+		}
+		
+		return dataByteArray;
+		
+		
+	}
+	
+	public static byte[] crearACK(byte[] blockNumber){
+		//Son dos bytes de blockNumber
+		byte[] ACK = { 0, OPCODE_ACK, blockNumber[0], blockNumber[1] };
+		
+		return ACK;
+		
+	}
+	
+	public static byte[] crearError(String errorMsg, byte[] errorCode){
+		
 
+		int errorLength = 2 + 2 + errorMsg.length() + 1;
+		
+		byte[] errorArray = new byte[errorLength];
+		
+		int position = 0; //recorre el array de bytes rrqByteArray
+		
+		//OPCODE
+		errorArray[position] = zero; //en la primera posicion esta el byte 0
+		position++;
+		errorArray[position] = OPCODE_ERROR;
+		position++;
+		//ERROR CODE
+		errorArray[position] = errorCode[0];
+		position++;
+		errorArray[position] = errorCode[1];
+		position++;
+		//ERROR MESSAGE
+		for (int i = 0; i < errorMsg.length(); i++) {
+			errorArray[position] = (byte) errorMsg.charAt(i); 
+			position++;
+		}
+		//ZERO
+		errorArray[position] = zero;
+		
+		return errorArray;
+	}
 }
